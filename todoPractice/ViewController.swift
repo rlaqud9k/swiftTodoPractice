73e0526916338207824cd26867c29cc29b979467
byeonggwankim <sakura.004p@example.com>
2022-06-05 02:23:17 +0900

    Initial Commit

---
+++ todoPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoPractice
+//
+//  Created by user on 2022/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
